d185c07ecd44240d663004976eb773be6a2e4a31
Rod0352 <92071426+Rod0352@users.noreply.github.com>
2021-12-30 13:15:51 -0500

    addition of inquirer module for user input

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-// const printProfileData = (profileDataArr) => {
-//     // This
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//         console.log(profileDataArr[i]);
-//     }
-//     console.log('Rodney Scott', 'Developer');
+const inquirer = require('inquirer');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-//     // Is the same as this
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
+// const pageHTML = generatePage(name, github);
 
-    
-
-// printProfileData(profileDataArgs);
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if(err) throw err;
-    console.log('Portfolio complete! Check out index.html to see the output');
-});
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if(err) throw err;
+//     console.log('Portfolio complete! Check out index.html to see the output');
+// });
